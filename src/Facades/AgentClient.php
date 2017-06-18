c949aa402014e4acf4f47ee274c55e8f718816a3
Elf Sundae <elf.sundae@gmail.com>
2017-06-19 03:07:37 +0800

    Add AgentClient facade

---
+++ src/Facades/AgentClient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ElfSundae\Laravel\Agent\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \ElfSundae\Laravel\Agent\Client
+ */
+class AgentClient extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'agent.client';
+    }
+}
