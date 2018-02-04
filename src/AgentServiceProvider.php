f6f925621ef4f1655cea19c5bf9f3e028e360a5e
Elf Sundae <elf.sundae@gmail.com>
2018-02-04 09:25:55 +0800

    Remove $defer

--- src/AgentServiceProvider.php
+++ src/AgentServiceProvider.php
@@ -7,13 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class AgentServiceProvider extends ServiceProvider
 {
     /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
      * Register the service provider.
      *
      * @return void
