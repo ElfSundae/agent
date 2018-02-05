b56eea1587332e81f7fe5138921c5b7e7a631402
Elf Sundae <elf.sundae@gmail.com>
2018-02-06 01:43:06 +0800

    Remove dymatic attributes accessor

--- src/Concerns/HasAttributes.php
+++ src/Concerns/HasAttributes.php
@@ -156,49 +156,4 @@ trait HasAttributes
     {
         $this->remove($offset);
     }
-
-    /**
-     * Dynamically check if an attribute is set.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function __isset($key)
-    {
-        return $this->offsetExists($key);
-    }
-
-    /**
-     * Dynamically retrieve the value of an attribute.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function __get($key)
-    {
-        return $this->offsetGet($key);
-    }
-
-    /**
-     * Dynamically set the value of an attribute.
-     *
-     * @param  string  $key
-     * @param  mixed   $value
-     * @return void
-     */
-    public function __set($key, $value)
-    {
-        $this->offsetSet($key, $value);
-    }
-
-    /**
-     * Dynamically unset an attribute.
-     *
-     * @param  string  $key
-     * @return void
-     */
-    public function __unset($key)
-    {
-        $this->offsetUnset($key);
-    }
 }
